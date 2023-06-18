d49e30a3f9ae61f2b504241cc8a3143fc41666eb
“Bunny” <spartha599@gmail.com>
2023-06-18 15:53:33 +0530

    Fixed wrong code

--- derp_sync.sh
+++ derp_sync.sh
@@ -1,6 +1,7 @@
 echo ....................................................................................../n
+cd
 mkdir derpfest
 cd derpfest
-git config -global user.email “spartha599@gmail.com” && git config –global user.name “bunny155”
+git config --global user.email “spartha599@gmail.com” && git config --global user.name “Bunny”
 repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 13
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
